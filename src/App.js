39565ef14ab66fb02c3c1b055771ba655015cd15
doniymankulov <ymankulovd@gmail.com>
2023-04-09 15:52:05 +0600

    btn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Main from "./components/main";
+import Comp from "./components/comp";
+import Prof from "./components/prof";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Hero/>
+            <Main/>
+            <Comp/>
+            <Prof/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/prof/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import col from './.././../IMG/icon-1.svg'
+import usb from './.././../IMG/icon-2.svg'
+import flesh from './.././../IMG/icon-3.svg'
+
+const Prof = ({dark}) => {
+    return (
+        <section style={{background: dark ? "#fff" : "black",}} id="prof">
+            <div className="container">
+                <div className="prof">
+                    <h1 className="text-white text-center text-4xl text-white pt-[10%] pl-[30px]">
+                        Получите профессию прямо сейчас
+                    </h1>
+                    <div className="flex pt-[10%] justify-around">
+                        <img src={col} alt="img"/>
+                        <img src={usb} alt="img"/>
+                        <img src={flesh} alt="img"/>
+                    </div>
+                    <div className="flex items-center">
+                        <div className="">
+                            <h4 className="text-white pt-[40px] text-center">Только практические <br/>
+                                навыки в работе
+                            </h4>
+                            <p className="text-gray-300 text-white pt-[50px] pl-[30px]">Lorem ipsum dolor sit amet,
+                                consectetur adipiscing elit. Netus eget velit quisque accumsan
+                                amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat dui.
+                            </p>
+                        </div>
+                        <div className="">
+                            <h4 className="text-white text-center pt-[40px] ">Работа на самом <br/>
+                                современном оборудовании
+                            </h4>
+                            <p className="text-gray-300 text-white pt-[40px]  pl-[30px]">Lorem ipsum dolor sit amet,
+                                consectetur adipiscing elit. Netus eget velit quisque accumsan
+                                amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat dui.
+                            </p>
+                        </div>
+                        <div className="">
+                            <h4 className="text-white text-center pt-[10px]">Сертификация <br/>
+                                по окончании обучения
+                            </h4>
+                            <p className="text-gray-300 text-white pt-[50px] pl-[30px]">Lorem ipsum dolor sit amet,
+                                consectetur adipiscing elit. Netus eget velit quisque accumsan
+                                amet tortor. Velit, volutpat egestas fringilla mi porttitor tempus. Placerat dui.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Prof;
